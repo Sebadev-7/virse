b315bcb76ec15a2d60aaced89fbfb2ddfde4c113
Sebadev7 <sebadev7@gmail.com>
2024-12-03 16:40:39 -0300

    probando tamaño de pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 import ReactPlayer from "react-player";
+import './index.css';
 import {
   Play,
   Pause,
